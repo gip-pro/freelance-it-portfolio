7b27dba34715c0b0e808f8f4f4099ac3806b30e4
Igor gip_pro <igor.g.pronkin@gmail.com>
2026-02-11 12:43:18 +0100

    Fix webhook domain

--- main.py
+++ main.py
@@ -210,11 +210,14 @@ def main():
     app.add_handler(CommandHandler("stop", stop))
     app.add_handler(CallbackQueryHandler(on_button))
 
+    domain = os.environ.get("RAILWAY_PUBLIC_DOMAIN")
+    port = int(os.environ.get("PORT", 8000))
+
     app.run_webhook(
         listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
-        webhook_url=f"https://{os.environ.get('RAILWAY_PUBLIC_DOMAIN')}"
-    )
+        port=port,
+        webhook_url=f"https://{domain}/telegram",
+        webhook_path="/telegram"
 
 if __name__ == "__main__":
     main()
